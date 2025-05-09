24ffb99300f4e6cae721d80ec98c96c0cc987fe5
Su Kara <62354767+sukara13@users.noreply.github.com>
2025-05-08 19:28:16 -0700

    Update install-certificate.sh
    update script

--- scripts/install-certificate.sh
+++ scripts/install-certificate.sh
@@ -1,6 +1,6 @@
-mkdir -p /usr/share/ca-certificates/mitmproxy
+sudo mkdir -p /usr/share/ca-certificates/mitmproxy
 sudo cp ~/.mitmproxy/mitmproxy-ca-cert.pem  /usr/share/ca-certificates/mitmproxy/mitmproxy-ca-cert.crt
-mkdir -p /usr/local/share/ca-certificates/mitmproxy
+sudo mkdir -p /usr/local/share/ca-certificates/mitmproxy
 sudo cp ~/.mitmproxy/mitmproxy-ca-cert.pem  /usr/local/share/ca-certificates/mitmproxy/mitmproxy-ca-cert.crt
 #sudo dpkg-reconfigure ca-certificates
 sudo update-ca-certificates
